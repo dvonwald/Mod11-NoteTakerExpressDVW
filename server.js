951860cdd4b47dbaacef8347a8d64bd208143144
Dylan <dylan.vonwald@gmail.com>
2022-06-19 22:34:07 -0600

    moved server.js out of public dir and into root dir because it wasn't working. created routes folder for eventual refactoring and modularization. i think a couple other small things i did too but can't remember

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
+//requiring in modules
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const uuid = require("./helpers/uuid");
 
+//PORT is either 3001 or whatever Heroku sets/needs to use
 const PORT = process.env.PORT || 3001;
 
+//storing the express function inside of the app variable
 const app = express();
 
 //Notes from class:
 //GET /notes should return the notes.html
 
 //GET * should return the index.html file
+//wild card route to direct users to index.html
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 //GET /api/notes should read the db.json and return all saved notes as JSON
 
 //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. Each note needs a unique ID.
+app.post;
 
-//Routes in  index.js
+//Routes referenced in  index.js
 //fetch /api/notes - GET
+app.get("");
 
 //fetch /api/notes - POST
 
 //fetch /api/notes - DELETE
-
-//wild card route to direct users to index.html
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.listen(PORT, () =>
   console.log(`App is listening at http://localhost:${PORT}`)
