7f1b7290723cb2dde25a25b201e28bb6b32fdb04
Dylan <dylan.vonwald@gmail.com>
2022-06-20 21:40:36 -0600

    got the read/dsiplay/get all notes route working, referenced mini project for express and used fsutils

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,43 @@
 const notes = require("express").Router();
+// requiring in filesystem module
+const fs = require("fs");
+
+//GET /notes should return the notes.html -- currently not working
+// notes.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "./public/notes.html"))
+// );
+
+//GET /api/notes should read the db.json and return all saved notes as JSON -- currently not working
+// notes.get("/", (req, res) => {
+//   fs.readFile(/db/db.json)
+//     .then((data) => res.json(JSON.parse(data)));
+// });
+
+// //Routes referenced in  index.js
+// //fetch /api/notes - GET
+// app.get("");
+
+// //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. Each note needs a unique ID.
+// //fetch /api/notes - POST
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add note`);
+
+  //object destructuring
+  const { title, text, id } = req.body;
+
+  const newNote = {
+    title,
+    text,
+    id: uuid(),
+  };
+  //have to conver the new object to a string to append to the JSON file
+  const noteString = JSON.stringify(newNote);
+
+  fs.appendFile(`/db/db.json`, noteString, (err) =>
+    err ? console.error(err) : console.log(`${newNote.title} saved to db.json`)
+  );
+});
+
+//fetch /api/notes - DELETE -- bonus  points
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const uuid = require("./helpers/uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("./helpers/fsUtils");
 
 //PORT is either 3001 or whatever Heroku sets/needs to use
 const PORT = process.env.PORT || 3001;
@@ -14,45 +19,31 @@ const app = express();
 app.use(express.static("public"));
 
 //Notes from class:
-//GET /notes should return the notes.html
+//GET /notes should return the notes.html -- working
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
+//GET /api/notes should read the db.json and return all saved notes as JSON
+// app.get("/api/notes", (req, res) => {
+//   fs.readFile("/db/db.json", "utf8", (err, data) => {
+//     .then((data) => res.json(JSON.parse(data));
+//   }
+// });
+
+// app.get("/api/notes", (req, res) => {
+//   fs.readFile("/db/db.json", "utf8", (err, data) => {
+//     res.json(JSON.parse(data));
+//   });
+// });
+
+app.get("/api/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
 //GET * should return the index.html file
 //wild card route to direct users to index.html
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
-
-//GET /api/notes should read the db.json and return all saved notes as JSON
-// app.get();
-
-// //Routes referenced in  index.js
-// //fetch /api/notes - GET
-// app.get("");
-
-// //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. Each note needs a unique ID.
-// //fetch /api/notes - POST
-app.post("/api/notes", (req, res) => {
-  console.info(`${req.method} request received to add note`);
-
-  //object destructuring
-  const { title, text, id } = req.body;
-
-  const newNote = {
-    title,
-    text,
-    id: uuid(),
-  };
-
-  //have to conver the new object to a string to append to the JSON file
-  const noteString = JSON.stringify(newNote);
-
-  fs.appendFile(`/db/db.json`, noteString, (err) =>
-    err ? console.error(err) : console.log(`${newNote.title} saved to db.json`)
-  );
-});
-
-//fetch /api/notes - DELETE -- bonus  points
 
 app.listen(PORT, () =>
   console.log(`App is listening at http://localhost:${PORT}`)
